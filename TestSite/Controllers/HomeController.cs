86f733ec166355d3961b0fab184749655fbf45d9
michael.sylvia <michael.sylvia@gmail.com>
2017-06-15 13:55:25 -0700

    Change to HomeController.cs

--- TestSite/Controllers/HomeController.cs
+++ TestSite/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TestSite.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your description page.";
 
             return View();
         }
